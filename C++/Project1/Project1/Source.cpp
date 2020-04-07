b51991b3fb6e7d523963020a15f26a794d62eeb5
hui <15395137457@163.com>
2020-04-07 21:53:04 +0800

    4.7

--- C++/Project1/Project1/Source.cpp
+++ C++/Project1/Project1/Source.cpp
@@ -4,29 +4,25 @@
 #include <vector>
 using namespace std;
 
-class sulution {
+class Base {
 public:
-	int removeDuplicates(vector<int>& nums) {
-		int n = nums.size();
-		if (n == 0) return 0;
-		int count = 1;
+	void test() {
+		cout << "Base" << endl;
+	}
+};
 
-		for (int i = 0; i < n-1; i++) {
-			int j = i + 1;
-			while(nums[j++] == nums[i]){}
-			nums[i + 1] = nums[j - 1];
-		}
+class Child : public Base {
+public:
+	void test() {
+		cout << "Child" << endl;
 	}
 };
 int main()
 {
-	
-	vector< vector <int> > a(3);
-	for (int i = 0; i < 3; i++) {
-		a[i].resize(3);
-	}
-	int n = a.size();
-	cout << n << endl;
+	Base *b = new Base;
+	Child *c = new Child;
+	b->test();
+	c->test();
 	return 0;
 }
 
